8d2ed08c541d653a4783c97854a744354ffb0b73
cams33-png <a01799837@tec.mx>
2025-11-10 22:30:44 -0600

    Programa hello

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//
+// Created by camal on 10/11/2025.
+//
+//Camila Alejandra Fuentes Lara A01799837
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::cout << "hola mundo!\n" << std::endl;
+    return 0;
+}
